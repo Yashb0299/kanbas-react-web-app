1f7d90c5143fac3c1abfcb3fc478333b112a72d4
Yashb0299 <yashb0299@gmail.com>
2024-05-23 02:16:15 -0400

    Update TOC.tsx

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -6,7 +6,7 @@ export default function TOC() {
             <li><a id="wd-lab2" href={"#/Labs/Lab2"}>Lab 2</a></li>
             <li><a id="wd-lab3" href={"#/Labs/Lab3"}>Lab 3</a></li>
             <li><a id="wd-kanbas" href={"#/Kanbas"}>Kanbas</a></li>
-            <li><a id="wd-github" href={"https://github.com/Yashb0299/kanbas-react-web-app"}>Github</a></li>
+            <li><a id="wd-github" href={"https://github.com/Yashb0299/kanbas-react-web-app/tree/a1"}>Github</a></li>
         </ul>
     );
 }

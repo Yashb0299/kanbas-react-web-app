c98d7104382001e1bb4ada332b3d156561cc6555
Yashb0299 <yashb0299@gmail.com>
2024-08-03 05:47:34 -0400

    Update index.tsx

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -9,6 +9,7 @@ import store from "./store";
 import { Provider } from "react-redux";
 import Account from "./Account";
 import ProtectedRoute from "./ProtectedRoute";
+import Session from "./Account/Session";
 
 export default function Kanbas() {
   const [courses, setCourses] = useState<any[]>([]);
@@ -55,6 +56,7 @@ export default function Kanbas() {
 
   return (
     <Provider store={store}>
+      <Session>
       <div id="wd-kanbas" className="h-100">
         <div className="d-flex h-100">
           <div className="d-none d-md-block bg-black">
@@ -93,6 +95,7 @@ export default function Kanbas() {
           </div>
         </div>
       </div>
+      </Session>
     </Provider>
   );
 }
